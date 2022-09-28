0697cd7266868e3666613ab4fe31f581f10ac5cc
Vincent CHABOSSON <chabosson@live.fr>
2022-09-28 15:20:37 +0200

    Added the first draft of the carousel with only the next button working

---
+++ index.js
@@ -0,0 +1,23 @@
+const carouselItem = document.querySelectorAll(".carouselItem");
+
+carouselItem.forEach((item, indx) => {
+    item.style.transform = `translateX(${indx * 100}%)`;
+});
+
+const nextItem = document.querySelector(".buttonNext");
+
+let currentItem = 0;
+let maxItem = carouselItem.length - 1
+
+
+nextItem.addEventListener("click", function () {
+    if (currentItem === maxItem) {
+        curentItem = 0
+    }
+    else {
+        currentItem++;
+    }
+    carouselItem.forEach((item, indx) => {
+        item.style.transform = `translateX(${100 * (indx - currentItem)}%)`;
+    });
+});
