8bbde87fe83dd4a97af82d376afbedb0c9969eb1
Vincent CHABOSSON <chabosson@live.fr>
2022-09-28 16:18:21 +0200

    Carousel is functional

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let maxItem = carouselItem.length - 1
 
 nextItem.addEventListener("click", function () {
     if (currentItem === maxItem) {
-        curentItem = 0
+        currentItem = 0
     }
     else {
         currentItem++;
@@ -21,6 +21,22 @@ nextItem.addEventListener("click", function () {
         item.style.transform = `translateX(${100 * (indx - currentItem)}%)`;
     });
 });
+
+const prevItem = document.querySelector(".buttonPrev");
+
+prevItem.addEventListener("click", function () {
+    if (currentItem === 0) {
+        currentItem = maxItem;
+    }
+    else {
+        currentItem--;
+    }
+    carouselItem.forEach((item, indx) => {
+        item.style.transform = `translateX(${100 * (indx - currentItem)}%)`;
+    });
+});
+
+
 const questions = [{
     question: "Quelle est cette ville ?",
     answer: ["Lyon", "Marseille", "Paris", "Toulouse"],
