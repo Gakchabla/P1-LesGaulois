0338d9a6b0519e83fd5a87184935c864d9ad3d35
Vincent CHABOSSON <chabosson@live.fr>
2022-09-29 15:05:44 +0200

    added some navigation dot and changed some css

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const nextItem = document.querySelector(".buttonNext");
 
 let currentItem = 0;
 let maxItem = carouselItem.length - 1
+const dotSwitch = function () {
+    if (currentItem === 0) {
+        dotUn.style.backgroundColor = "white";
+        dotDeux.style.backgroundColor = "rgba(211, 211, 211, 0.39)";
+        dotTrois.style.backgroundColor = "rgba(211, 211, 211, 0.39)";
+    }
+    else if (currentItem === 1) {
+        dotUn.style.backgroundColor = "rgba(211, 211, 211, 0.39)";
+        dotDeux.style.backgroundColor = "white";
+        dotTrois.style.backgroundColor = "rgba(211, 211, 211, 0.39)";
+    }
+    else if (currentItem === 2) {
+        dotUn.style.backgroundColor = "rgba(211, 211, 211, 0.39)";
+        dotDeux.style.backgroundColor = "rgba(211, 211, 211, 0.39)";
+        dotTrois.style.backgroundColor = "white";
+    }
+}
 
 
 nextItem.addEventListener("click", function () {
@@ -22,6 +39,7 @@ nextItem.addEventListener("click", function () {
     carouselItem.forEach((item, indx) => {
         item.style.transform = `translateX(${100 * (indx - currentItem)}%)`;
     });
+    dotSwitch();
 });
 
 const prevItem = document.querySelector(".buttonPrev");
@@ -36,7 +54,39 @@ prevItem.addEventListener("click", function () {
     carouselItem.forEach((item, indx) => {
         item.style.transform = `translateX(${100 * (indx - currentItem)}%)`;
     });
+    dotSwitch();
 });
+const dotUn = document.getElementById("dot1")
+
+dotUn.addEventListener("click", function () {
+    currentItem = 0;
+    carouselItem.forEach((item, indx) => {
+        item.style.transform = `translateX(${100 * (indx - currentItem)}%)`;
+    });
+    dotSwitch();
+});
+
+const dotDeux = document.getElementById("dot2")
+
+dotDeux.addEventListener("click", function () {
+    currentItem = 1;
+    carouselItem.forEach((item, indx) => {
+        item.style.transform = `translateX(${100 * (indx - currentItem)}%)`;
+    });
+    dotSwitch();
+});
+
+const dotTrois = document.getElementById("dot3")
+
+dotTrois.addEventListener("click", function () {
+    currentItem = 2;
+    carouselItem.forEach((item, indx) => {
+        item.style.transform = `translateX(${100 * (indx - currentItem)}%)`;
+    });
+    dotSwitch();
+});
+dotSwitch();
+
 
 // **************************Quiz****************************
 
